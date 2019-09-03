f5c40de4d56934a756bcd39467b7d84866773bcf
Boyan <bojanlzc@gmail.com>
2019-09-04 02:39:39 +0300

    Preparing for Tutorial 2

--- Priprema za test/Predavanje_2a/main.cpp
+++ Priprema za test/Predavanje_2a/main.cpp
@@ -5,26 +5,27 @@
 
 int main()
 {
-    /*bool a(true);
+    //bool a(true);
     //std::cout << std::boolalpha << a << " " << false << " " << (2 < 3) << std::endl;
 
-    enum Dani {Ponedjeljak = 2, Utorak, Srijeda, Cetvrtak, Petak, Subota, Nedjelja};
-    Dani danas, sutra;
+//    enum class Dani {Ponedjeljak = 2, Utorak, Srijeda, Cetvrtak, Petak, Subota, Nedjelja};
+//    Dani danas, sutra;
+//
+//    enum Rezultat {Poraz = 3, Nerijeseno, Pobjeda = 7} danasnji_rezultat;
+//    //enum {Poraz, Nerijeseno, Pobjeda} danasnji_rezultat;
+//    danasnji_rezultat = Pobjeda;
+//    danas = Dani::Ponedjeljak;
+//    //danas = Dani(8);
 
-    enum Rezultat {Poraz = 3, Nerijeseno, Pobjeda = 7} danasnji_rezultat;
-    //enum {Poraz, Nerijeseno, Pobjeda} danasnji_rezultat;
-    danasnji_rezultat = Rezultat(0);
-    danas = Dani::Ponedjeljak;
-    //danas = Dani(danas+1);
+//    std::cout << danasnji_rezultat;
 
-    std::cout << danasnji_rezultat;*/
+    //std::complex<double>z(2, 3.5);
 
-    /*std::complex<double>z(2, 3.5);
-
-    z = std::complex<double>(5, z.imag());
+    //z = std::complex<double>(5, z.imag());
 
     //std::cout << z;
-    typedef std::complex<double> Kompleksni;
+
+    /*typedef std::complex<double> Kompleksni;
     double ss(-25.0);
     std::complex<double> s;
 
@@ -32,11 +33,11 @@ int main()
 
     std::cout << s;*/
 
-    std::complex<double>z;
+    /*std::complex<double>z;
 
     z = {1, 2};
 
     std::cout << z;
-
+*/
     return 0;
 }
