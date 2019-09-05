07b8d4840e609ac0704d045beb2f6ab185e7dbdc
Boyan <bojanlzc@gmail.com>
2019-09-06 01:17:17 +0300

    Tutorial 4 finished

--- Priprema za test/Tutorijal 4/main.cpp
+++ Priprema za test/Tutorijal 4/main.cpp
@@ -27,7 +27,7 @@ void Izvrni(std::string &s){
     }
 
 }*/
-/*template <typename NekiTip>
+/*
 void UnosBroja(std::string s1, std::string s2, NekiTip &n){
 
     std::cout << s1;
@@ -41,10 +41,67 @@ void UnosBroja(std::string s1, std::string s2, NekiTip &n){
     std::cout << std::endl << n << std::endl;
 }*/
 
+/*void Cifre(int broj, int &a, int &b){
+    a = 9, b = 0;
+    while(broj > 0){
+        if(broj%10<a)a = broj%10;
+        if(broj%10>b)b = broj%10;
+        broj/=10;
+    }
+}*/
+
+
+/*template <typename Neki>
+
+void UnesiVektor(std::vector<Neki> &v1){
+    Neki broj;
+    while(std::cin >> broj, broj != -1) v1.push_back(broj);
+}
+
+std::vector<Neki> Presjek(const std::vector<Neki>v1, const std::vector<Neki>v2){
+    std::vector<Neki> v3;
+
+    int j(0);
+    for(int i(0); i<v1.size(); i++){
+        for(j = i+1; j<v1.size(); j++){
+            if(v1[i] == v1[j])break;
+        }
+        if(j == v1.size())v3.push_back(v1[i]);
+    }
+
+    j=0;
+
+    for(int i(0); i<v2.size(); i++){
+        for(j = 0; j<v3.size(); j++){
+            if(v2[i] == v3[j])break;
+        }
+        if(j == v3.size())v3.push_back(v2[i]);
+    }
+
+    return v3;
+}
+
+*/
+
+/*void UnosBroja(std::string s1, std::string s2, Neki &prvi_broj){
+
+    std::string b;
+    std::cout << s1;
+    std::cin >> prvi_broj;
+    std::getline(std::cin, b);
+
+    while(b != ""){
+        std::cout << s2;
+
+        std::cin >> prvi_broj;
+        std::getline(std::cin, b);
+        std::cout << b;
+    }
+}*/
+
+
 int main()
 {
-
-
     /*int prvi_broj;
     double drugi_broj;
     bool is;
@@ -105,5 +162,73 @@ int main()
 
     std::cout << s;*/
 
+    /*Zadatak 1
+
+    int broj_rijeci;
+    std::cin >> broj_rijeci;// >> std::ws;
+    std::cin.ignore(10000, '\n');
+    std::vector<std::string> vs, nvs;
+    int i(0), j;
+    std::string sa, sz;
+
+    while(broj_rijeci > i){
+        vs.resize(i+1);
+        j=0;
+        std::getline(std::cin, vs[i]);
+
+        for(j = 0; j<i; j++){
+            if(vs[i] == nvs[j])break;
+        }
+
+        if(i == j)nvs.push_back(vs[i]);
+
+        if(i==0){
+            sa = vs[0];sz = vs[0];
+        }
+        else{
+            if(strcmp(vs[i].c_str(), sa.c_str()) == -1) sa = vs[i];
+            if(strcmp(vs[i].c_str(), sz.c_str()) == 1) sz = vs[i];
+        }
+        i++;
+    }
+
+    std::cout << "First is " << sa << std::endl << "Second is " << sz << std::endl;
+
+    for(std::string s : nvs) std::cout << s << std::endl;
+    */
+
+    //for(std::string a : vs) std::cout << a << " ";
+    //std::string s1{"c"}, s2{"b"};
+    //std::cout << strcmp(s1.c_str(), s2.c_str());
+
+    /*zadatak 2
+    int a, b;
+    Cifre(37232645, a, b);
+
+    std::cout << "Min: " << a << std::endl << "Max: " << b;*/
+
+    /*zadatak 4
+    int n;
+    double x;
+    UnosBroja("Unesi prvi broj: ", "Neispravan unos!\n", n);
+    UnosBroja("Unesi prvi broj: ", "Neispravan unos!\n", x);
+
+    std::cout << "\nx^n =  " << std::pow(x, n);*/
+
+    /*zadatak 5
+    //std::vector<int> v1, v2;
+  //  std::vector<std::string> v1{"Ja", "Ti", "Ti", "On"}, v2{"Ti","On","Ona","Ono"};
+
+    //UnesiVektor(v1);
+    //UnesiVektor(v2);
+
+    auto v3 = Presjek(v1, v2);
+
+    for(auto x: v1) std::cout << x << " ";
+    std::cout << std::endl;
+    for(auto x: v2) std::cout << x << " ";
+    std::cout << std::endl;
+    for(auto x: v3) std::cout << x << " ";
+*/
     return 0;
 }
