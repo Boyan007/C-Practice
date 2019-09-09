1e8c4dd235c8649132dbd060a7e09814458589d7
Boyan <bojanlzc@gmail.com>
2019-09-09 15:51:02 +0300

    Preparing for tutorial 7

--- Priprema za test/Predavanje 7a/main.cpp
+++ Priprema za test/Predavanje 7a/main.cpp
@@ -3,27 +3,64 @@
 #include <memory>
 //using namespace std;
 
-bool IspredPo_ASCII_Kodovima(const char *a, const char *b) {
+/*bool IspredPo_ASCII_Kodovima(const char *a, const char *b) {
     return std::strcmp(a, b) < 0;
-}
+}*/
 
 int main()
 {
-    char prva[10]{'J', 'e', 'd', 'a', 'n'}, druga[10]{'d', 'e', 'd', 'a', 'n'};
-    std::cout << 'J' - 'D' << endl;
 
-    const char *imena[5] = {"Maja", "Tarik", "Ivo", "Belma", "Sead"};
-    std::sort(imena, imena + 5);//poredi adrese pokazivaca
+
+    //char prva[10]{'J', 'e', 'd', 'a', 'n'}, druga[10]{'d', 'e', 'd', 'a', 'n'};
+    //std::cout << 'J' - 'D' << std::endl;
+/*
+    int a[3] = {1,2,3}, *p1, *p2, *temp;
+
+    p1 = &a[0]; p2 = &a[1];
+
+    temp = p1; p1 = p2; p2 = temp;
+
+    std::cout << *p1 << " " << *p2;
+
+    std::cout << a[0];*/
+
+    /*char niz[2][5] = {{'J','E','D','A','N'},{'D','E','V','E','T'}};
+
+    char *pok[2], *temp, pr;
+
+    pok[0] = &niz[0][0];
+    pok[1] = &niz[1][0];
+
+    temp = pok[0];
+    pok[0] = pok[1];
+    pok[1] = temp;
+
+    std::cout << *pok[0] << std::endl << std::endl << std::endl << std::endl;
+
+    for(int i(0); i<2; i++){
+        for(int j(0); j<5; j++){
+            std::cout << niz[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }*/
+
+
+
+    //std::cout << *p;
+    /*std::sort(imena, imena + 5);//poredi adrese pokazivaca
     std::sort(imena, imena + 5, IspredPo_ASCII_Kodovima);
     std::sort(imena, imena + 5, [](const char *a, const char *b) {
         return std::strcmp(a, b) < 0;
     });
 
+    std::shared_ptr<double> p(new double(3.5));//isto sto i double*p(new double(5))*/
 
-    std::shared_ptr<double> p(new double(3.5));
 
-    /*(auto umjesto std::shared_ptr<double>)*/std::shared_ptr<double> p(std::make_shared<double>(3.5));
-    p = std::make_shared<double>(2.13);
+
+    /*(auto umjesto std::shared_ptr<double>)*//*std::shared_ptr<double> p(std::make_shared<double>(3.5));
+    p = std::make_shared<double>(2.13);*/
+
+
 
 
     return 0;
--- Priprema za test/Predavanje 7b/main.cpp
+++ Priprema za test/Predavanje 7b/main.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
+#include <list>
+#include <set>
+#include <deque>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <functional>
 
-using namespace std;
+//using namespace std;
+/*
+void Tabeliraj(std::function<int(int)> f, int x_min, int x_max) { //sada radi kako treba
+    for(int x = x_min; x <= x_max; x++)
+    std::cout << std::setw(10) << x << std::setw(10) << f(x) << std::endl;
+}
+
+template <typename FunkcijskiTip>
+void Tabeliraj(FunkcijskiTip f, int x_min, int x_max) {
+    for(int x = x_min; x <= x_max; x++)
+    std::cout << std::setw(10) << x << std::setw(10) << f(x) << std::endl;
+}
+
+*/
+
+/*void IspisiSumu(std::function<int(int, int)> f, int a, int b){
+    std::cout << f(a, b);
+}*/
+
+/*std::function<double(double, double)>CeSabrati(int a, int b, int c){
+    return [a](double x, double y){return x+y+a;};
+}*/
+/*
+IspisiSumu(int a, int b, int c){
+     int (*pok_na_fun)(int, int), d(a);
+     pok_na_fun = [=](int i, int j){return i + j + d;};
+     std::cout << pok_na_fun(b, c);
+}*/
 
 int main()
 {
-    std::unique_ptr<double> p(new double(3.5));
+    //auto Saberi(CeSabrati(1, 2, 3));
+    //std::cout << Saberi(4, 5);
+
+
+    /*std::unique_ptr<double> p(new double(3.5));
     p = std::unique_ptr<double>(new double(2.13)); // Preusmjeravanje...
 
     std::unique_ptr<int[]> p(new int[5]); // Obratiti paţnju na []...
@@ -21,8 +59,41 @@ int main()
         std::cout << *it++ << " "; // Prva 3 elementa
     std::cout << std::endl; // Novi red
     while(it != lista.end())
-        std::cout << *it++ << " "; // Ostali elementi
+        std::cout << *it++ << " "; // Ostali elementi*/
+    /*std::list<int>l{2, 3, 4};
 
+    l.push_back(5);
+    l.push_front(1);
+
+
+    std::list<int>::iterator it(l.begin());
+
+    it++;
+    it++;
+
+    it = l.erase(it);
+    //it--;
+    l.insert(it, 3);
+
+    for(int x: l)std::cout << x << " ";*/
+
+    /*std::set<int> skup{3, 5, 1, 4, 3, 7};
+    for(int x : skup) std::cout << x << " "; // Ispis 1 3 4 5 7
+
+    std::cout << std::endl;
+
+    std::multiset<int> multiskup{3, 5, 1, 4, 3, 7};
+    for(int x : multiskup) std::cout << x << " ";*/ // Ispis 1 3 3 4 5 7
+
+    /*std::deque<int> d{3, 5, 2, 8, 4};
+    std::vector<int> v;
+    std::copy(d.begin(), d.end(), std::back_inserter(v));*/
+
+    /*std::ostream_iterator<int> it(std::cout, " ");
+    *it++ = 3; *it++ = 2; *it++ = 7;*/
+
+    //int a(1);
+    //Tabeliraj([a](int x) { return x * x + a; }, 0, 10);//sada radi kako treba
 
     return 0;
 }
