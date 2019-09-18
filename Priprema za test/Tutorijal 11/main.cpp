b08a4f0eafa9aed81249b6f6e309943186f47f88
Boyan <bojanlzc@gmail.com>
2019-09-18 17:18:12 +0300

    Tutorijal 11, 2/3 zadatka zavrseni

--- Priprema za test/Tutorijal 11/main.cpp
+++ Priprema za test/Tutorijal 11/main.cpp
@@ -2,6 +2,8 @@
 #include <cstring>
 #include <iomanip>
 #include <algorithm>
+#include <initializer_list>
+#include <exception>
 //using namespace std;
 
 class Tim{
@@ -49,20 +51,25 @@ public:
     explicit Liga(int velicina_lige);
     explicit Liga(std::initializer_list<Tim> lista_timova);
     ~Liga();
-    Liga(const Liga &l);//konstruktor kopije, parametri funkcije postaju kpije originala, ove kopije
-    Liga(Liga &&l);//pomjerajuci konstruktor, ako je u pitanju npr funkcija, ono sto vraca je privremena vrijednost, nije l-vrijednost, nego r
+    Liga(const Liga &l);//konstruktor kopije, parametri funkcije postaju kopije originala, ove kopije
+    Liga(Liga &&l);//pomjerajuci konstruktor, ako je u pitanju npr funkcija, ono sto prima je privremena vrijednost, nije l-vrijednost, nego r
     Liga &operator =(const Liga &l);//kopirajuci operator dodjele, sluzi za dodjelu l-vrijednosti
     Liga &operator =(Liga &&l);//pomjerajuci operator dodjele; sluzi za dodjelu r-vrijednosti
 
     void DodajNoviTim(const char ime_tima[]);
     void RegistrirajUtakmicu(const char tim1[], const char tim2[], int rezultat_1, int rezultat_2);
     void IspisiTabelu();
+
+//    Tim *begin() const { return timovi; }
+//    Tim *end() const { return timovi + broj_timova; }
 };
 
 Liga::Liga(int velicina_lige): max_br_timova(velicina_lige), broj_timova(0), timovi(new Tim*[max_br_timova]){}
 
-Liga::Liga(std::initializer_list<Tim> lista_timova): broj_timova(lista_timova.size()), max_br_timova(lista_timova.size()){
-    timovi = new Tim*[lista_timova.size()];
+Liga::Liga(std::initializer_list<Tim> lista_timova): broj_timova(lista_timova.size()), max_br_timova(lista_timova.size()), timovi(new Tim*[lista_timova.size()]){
+    //std::copy(lista_timova.begin(), lista_timova.end(), *timovi); // ne moze jer treba alocirati nove elemente, inicijalizaciona lista ne radi sa pokazivacima i ne daje adrese, zato nema ni operatora +
+    int i(0);
+    for(Tim x: lista_timova)timovi[i++] = new Tim(x); // kada imamo dvostruke pokazivace, svaki leement treba alocirati, a ne dodjeliti; zato ne radi copy
 }
 
 Liga::~Liga(){
@@ -126,6 +133,8 @@ void Liga::RegistrirajUtakmicu(const char tim1[], const char tim2[], int rezulta
 }
 
 void Liga::IspisiTabelu(){
+    std::sort(timovi, timovi+broj_timova, [](const Tim *t1, const Tim *t2){return strcmp(t1->DajImeTima(), t2->DajImeTima()) < 0;});
+    std::sort(timovi, timovi+broj_timova, [](const Tim *t1, const Tim *t2){return t1->DajBrojPoena() > t2->DajBrojPoena();});
     for(int i(0); i<broj_timova; i++){
         timovi[i]->IspisiPodatke();
         std::cout << std::endl;
@@ -141,7 +150,22 @@ int main()
     std::cout << std::endl << Jedan.DajImeTima() << std::endl;
     std::cout << Jedan.DajBrojPoena() << std::endl << Jedan.DajGolRazliku();*/
 
+    /*zadatak 2
+    Liga liga(5);
+    liga.DodajNoviTim("Celik");
+    liga.DodajNoviTim("Jedinstvo");
+    liga.DodajNoviTim("Zeljeznicar");
+    liga.DodajNoviTim("Velez");
+    liga.DodajNoviTim("Sarajevo");
 
+    liga.RegistrirajUtakmicu("Celik", "Velez", 2, 3);
+    liga.RegistrirajUtakmicu("Celik", "Sarajevo", 1, 1);
+    liga.RegistrirajUtakmicu("Sarajevo", "Zeljeznicar", 0, 0);
+    liga.RegistrirajUtakmicu("Zeljeznicar", "Jedinstvo", 1, 0);
+    liga.RegistrirajUtakmicu("Celik", "Jedinstvo", 1, 1);
+
+    Liga liga{"Celik", "Sarajevo", "Jedinstvo", "Zeljeznicar", "Velez"};
+    liga.IspisiTabelu();*/
 
     return 0;
 }
