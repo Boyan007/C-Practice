350f1d96305d524d72f60a38ba3db529f8990a4e
Boyan <bojanlzc@gmail.com>
2019-09-23 00:56:41 +0300

    Tutorijal 13 u potpunosti gotov, detalji sada

--- Priprema za test/Tutorijal 13/main.cpp
+++ Priprema za test/Tutorijal 13/main.cpp
@@ -2,9 +2,10 @@
 #include <string>
 #include <cmath>
 #include <memory>
+#include <algorithm>
 //using namespace std;
 
-/*zadatak 1*/
+/*zadatak 1
 
 class Lik{
     std::string naziv;
@@ -114,16 +115,83 @@ KolekcijaLikova &KolekcijaLikova::operator =(const KolekcijaLikova &k) {
         for(int i = 0; i < broj_likova; i++) likovi[i] = k.likovi[i]->DajKopiju();
     }
     return *this;
-}
+}*/
+class Student {
+    std::string ime;
+    int indeks;
+public:
+    Student(std::string ime, int ind) : ime(ime), indeks(ind) {}
+    std::string DajIme() const { return ime; }
+    int DajIndeks() const { return indeks; }
+    virtual void Ispisi() const {
+        std::cout << std::endl << "Student " << ime << " ima indeks " << indeks;
+    }
+};
+class DiplomiraniStudent : public Student {
+    int godina_diplomiranja;
+public:
+    DiplomiraniStudent(std::string ime, int ind, int god_dipl) : Student(ime, ind), godina_diplomiranja(god_dipl) {}
+    int DajGodinuDiplomiranja() const { return godina_diplomiranja; }
+    void Ispisi() const {
+        Student::Ispisi();
+        std::cout << ", a diplomirao je " << godina_diplomiranja << ". godine" << std::endl;
+    }
+};
 
+class StudentskaSluzba{
+    int broj_studenata;
+    const int kapacitet;
+    Student **studenti;
+public:
+    StudentskaSluzba(int kapacitet): kapacitet(kapacitet), broj_studenata(0), studenti(new Student*[kapacitet]){}
+    ~StudentskaSluzba();
+    StudentskaSluzba(const StudentskaSluzba &Ss) = delete; //zabrana kopiranja
+    StudentskaSluzba &operator =(const StudentskaSluzba &Ss) = delete;//zabrana dodjele
+    void UpisStudenta(std::string ime_i_prezime, int broj_indexa);
+    void UpisDiplomiranogStudenta(std::string ime_i_prezime, int broj_indexa, int godina_diplomiranja);
+    void IspisiPoAbecedi()const;
+    void IspisiPoIndexu()const;
+};
+StudentskaSluzba::~StudentskaSluzba(){
+    for(int i(0); i<broj_studenata; i++) delete studenti[i];
+    delete[] studenti;
+}
+void StudentskaSluzba::UpisStudenta(std::string ime_i_prezime, int broj_indexa){
+    if(broj_studenata>=kapacitet)throw std::range_error("Maksimalan broj studenata dostignut");
+    studenti[broj_studenata++] = new Student(ime_i_prezime, broj_indexa);
+}
+void StudentskaSluzba::UpisDiplomiranogStudenta(std::string ime_i_prezime, int broj_indexa, int godina_diplomiranja){
+    if(broj_studenata>=kapacitet)throw std::range_error("Maksimalan broj studenata dostignut");
+    studenti[broj_studenata++] = new DiplomiraniStudent(ime_i_prezime, broj_indexa, godina_diplomiranja);
+}
+void StudentskaSluzba::IspisiPoAbecedi()const{
+    std::sort(studenti, studenti+broj_studenata, [](Student *a, Student *b){return a->DajIme() < b->DajIme();});
+    for(int i(0); i<broj_studenata; i++)studenti[i]->Ispisi();
+}
+void StudentskaSluzba::IspisiPoIndexu()const{
+    std::sort(studenti, studenti+broj_studenata, [](Student *a, Student *b){return a->DajIndeks() < b->DajIndeks();});
+    for(int i(0); i<broj_studenata; i++)studenti[i]->Ispisi();
+}
 int main()
 {
+    StudentskaSluzba s(3);
+    s.UpisStudenta("Ivan Ivanovic", 1238);
+    s.UpisDiplomiranogStudenta("Tomo Tomovic", 1236, 2015);
+    s.UpisStudenta("Pero Perovic", 1335);
+
+    s.IspisiPoAbecedi();
+    s.IspisiPoIndexu();
+    //s = 6; // provjeriti sta bi bilo za definisano kopiranje i dodjelu
+
+    /*zadatak 1
     KolekcijaLikova likovi(3);
 
     likovi.DodajKrug(5);
     likovi.DodajPravougaonik(3,4);
     likovi.DodajTrougao(3, 4, 5);
 
-    likovi.IspisiKolekciju();
+    likovi.IspisiKolekciju();*/
+
+
     return 0;
 }
